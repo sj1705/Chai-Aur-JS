a4accbda86ed5a399f8f9ea43f126003cea58643
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2024-04-09 21:24:37 +0530

    Changes to be committed: 	new file:   06_num_and_maths.js

---
+++ 06_num_and_maths.js
@@ -0,0 +1,23 @@
+const num=400;
+
+console.log(num);
+
+const num2= new Number(1000)
+
+console.log(num2);
+
+console.log(typeof num2.toString());
+
+console.log(typeof num2.toString().length);
+
+console.log( num2.toString().length);
+
+console.log(num2.toFixed(2));
+
+const othernum= 2345.53
+
+console.log(othernum.toPrecision(5));
+
+const hundreds=10000000
+
+console.log(hundreds.toLocaleString('en-IN')); // for having comma as per indian standards
